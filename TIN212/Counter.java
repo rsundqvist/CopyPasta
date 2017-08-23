9617a0cc31d8b1f55f85ebec0c70a72b83048b71
Richard Sundqvist <richard.sundqvist@live.se>
2017-08-23 11:12:42 +0200

    Counter.java, used in scope.

---
+++ TIN212/Counter.java
@@ -0,0 +1,19 @@
+public class Counter { 
+    private int counterValue = 0;                                                    
+                                                                                                                       
+    public void increment () {
+        setCounterValue(counterValue + 1);              
+    }   
+    public void increment (int value) {
+
+        int temp = counterValue + value;
+        setCounterValue(temp);
+    }
+
+    public void setCounterValue(int counterValue) {
+        this.counterValue = counterValue;
+    }
+    public void decrement () {
+        setCounterValue(counterValue - 1);              
+    }
+}

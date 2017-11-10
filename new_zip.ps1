d06ca50e39bb3f7f5ade143af2e5ad475d4729e6
Richard Sundqvist <whisp91@hotmail.com>
2017-11-10 19:02:32 +0100

    zip, update zip ps script

--- new_zip.ps1
+++ new_zip.ps1
@@ -1,4 +1,6 @@
 Add-Type -A System.IO.Compression.FileSystem
+rm .\CopyPasta.zip
 [IO.Compression.ZipFile]::CreateFromDirectory('.\classes\artifacts\CopyPasta', 'CopyPasta.zip')
-# [IO.Compression.ZipFile]::ExtractToDirectory('foo.zip', 'bar')
-# mv .\CopyPasta.zip .\classes\artifacts\
+#[IO.Compression.ZipFile]::ExtractToDirectory('foo.zip', 'bar')
+#mv .\CopyPasta.zip .\classes\artifacts\
+#pause
